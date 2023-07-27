dc4f3cd3ccb74da08d09547c2a2f3024cc6fca10
微笑 <wang1234561211@outlook.com>
2023-07-27 11:33:45 +0800

    feat: 配置neo-tree

--- lua/config/setup.lua
+++ lua/config/setup.lua
@@ -11,4 +11,46 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup()
+require("lazy").setup({
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      local tree = require("neo-tree")
+      tree. setup({
+        close_if_last_window = true,
+        enable_git_status = true,
+        use_libuv_file_watcher = true,
+        follow_current_file = true,
+        window = {
+          width = 30,
+          mappings = {
+            ["m"] = {
+              "move",
+              config = {
+                show_path = "absolute"
+              }
+            }
+          }
+        },
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_by_name = {
+              "node_modules",
+              ".git"
+            }
+          }
+        }
+      })
+
+      vim.api.nvim_set_keymap('n', '<C-t>', ':Neotree toggle<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<C-f>', ':Neotree reveal<CR>', { noremap = true, silent = true })
+    end
+  }
+})
