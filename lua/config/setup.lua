aba7c16e5a875cd17d968ebf0c7721b5eb0df6d8
微笑 <wang1234561211@outlook.com>
2023-07-27 14:09:23 +0800

    feat: 新增telescope

--- lua/config/setup.lua
+++ lua/config/setup.lua
@@ -59,9 +59,26 @@ require("lazy").setup({
     },
     config = function()
       vim.g.lazygit_floating_window_scaling_factor = 1.0
-			vim.g.lazygit_floating_window_winblend = 0
-			vim.g.lazygit_use_neovim_remote = true
+      vim.g.lazygit_floating_window_winblend = 0
+      vim.g.lazygit_use_neovim_remote = true
       vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
     end
   },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local m = { noremap = true, nowait = true }
+      local builtin = require('telescope.builtin');
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, m)
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, m)
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, m)
+      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, m)
+      vim.keymap.set('n', '<leader>fc', builtin.commands, m)
+      vim.keymap.set('n', '<leader>fh', builtin.search_history, m) -- 注意: 'search_history' 可能不是 'telescope.builtin' 中的有效函数，你需要替换为正确的函数
+      vim.keymap.set('n', '<leader>fm', builtin.git_status, m)
+      vim.keymap.set('n', '<leader>fe', builtin.resume, m)
+    end
+  }
 })
