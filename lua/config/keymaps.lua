9d03b49207682dfd98aa047f3923345ddb888225
微笑 <wang1234561211@outlook.com>
2023-11-09 13:12:55 +0800

    feat: 配置调整

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+vim.api.nvim_set_keymap("n", "<Leader>cb", "", {
+  noremap = true,
+  silent = true,
+  callback = function()
+    local language = vim.fn.input("Language: ")
+    -- 判断输入是否为空，为空则不添加语言
+    local lines = language ~= "" and { "```" .. language, "", "```" } or { "```", "", "```" }
+    vim.api.nvim_put(lines, "l", true, true)
+    vim.api.nvim_command("normal! k")
+  end,
+})
