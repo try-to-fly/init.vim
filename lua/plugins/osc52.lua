95e6681e43e8c7338b5d690e7d742dcf4979d609
微笑 <wang1234561211@outlook.com>
2023-12-21 11:59:54 +0800

    fix: 修复osc52复制bug

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -3,6 +3,6 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>c", require("osc52").copy_operator, { expr = true })
     vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
-    vim.keymap.set("v", "<leader>c", require("osc52").copy_visual)
+    vim.keymap.set("v", "<leader>cc", require("osc52").copy_visual)
   end,
 }
