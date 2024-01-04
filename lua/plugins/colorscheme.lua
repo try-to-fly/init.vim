1680e8d8cd4d008c7c31065502f829ad9451a89c
微笑 <wang1234561211@outlook.com>
2024-01-04 18:55:37 +0800

    feat: 新增tokyodrak

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,19 @@
 return {
-  "catppuccin/nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme catppuccin-mocha]])
-  end,
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin-mocha]])
+    end,
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+      -- custom options here
+    },
+    config = function(_, opts)
+      require("tokyodark").setup(opts) -- calling setup is optional
+      -- vim.cmd([[colorscheme tokyodark]])
+    end,
+  },
 }
