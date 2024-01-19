5202022364668bc5659ce0d4e240a250362bba44
微笑 <wang1234561211@outlook.com>
2024-01-19 13:00:49 +0800

    feat: 新增advanced-git-search.nvim

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -25,4 +25,21 @@ return {
       require("telescope").load_extension("package_info")
     end,
   },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          advanced_git_search = {},
+        },
+      })
+
+      require("telescope").load_extension("advanced_git_search")
+    end,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-fugitive",
+      "tpope/vim-rhubarb",
+    },
+  },
 }
