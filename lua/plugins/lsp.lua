93ada3ac04424ba610dc0a32e8243371685a8541
微笑 <wang1234561211@outlook.com>
2024-01-28 11:54:09 +0800

    feat: nvim-lsp-file-operations，文件重命名

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -54,4 +54,13 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
