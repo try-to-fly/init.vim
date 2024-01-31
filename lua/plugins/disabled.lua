6cfdc0d9cc005da142700dc6d009b710e25f1301
微笑 <wang1234561211@outlook.com>
2024-01-31 21:00:22 +0800

    feat: 使用内置的markdown配置

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "iamcco/markdown-preview.nvim", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
 }
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,10 @@
 return {
   { "jxnblk/vim-mdx-js" },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman" })
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman" })
+  --   end,
+  -- },
 }
