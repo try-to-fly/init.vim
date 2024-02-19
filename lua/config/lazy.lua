69576b3bfbba57a27a52c0d000ffb7ed7fbb886c
微笑 <wang1234561211@outlook.com>
2024-02-19 09:26:35 +0800

    feat: 开启codeium

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
