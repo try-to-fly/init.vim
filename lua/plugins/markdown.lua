ad52b2c056ded917e52205c7fd40569a10f51605
微笑 <wang1234561211@outlook.com>
2024-03-04 13:18:48 +0800

    feat: 新增markdown插件

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,21 @@
 return {
   { "jxnblk/vim-mdx-js" },
+  {
+    "SidOfc/mkdx",
+    config = function()
+      -- 使用 Lua 配置 mkdx 插件（示例，根据实际情况调整）
+      vim.g["mkdx#settings"] = {
+        ["highlight"] = { ["enable"] = 1 },
+        ["enter"] = { ["shift"] = 1 },
+        ["links"] = { ["external"] = { ["enable"] = 1 } },
+        ["toc"] = { ["text"] = "Table of Contents", ["update_on_write"] = 1 },
+        ["fold"] = { ["enable"] = 1 },
+      }
+
+      -- 禁用 vim-polyglot 中的 markdown 支持
+      vim.g.polyglot_disabled = "markdown"
+    end,
+  },
   -- {
   --   "williamboman/mason.nvim",
   --   opts = function(_, opts)
