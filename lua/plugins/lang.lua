e942b7a04eab71e6f17ef36a9780d325b61506e3
微笑 <wang1234561211@outlook.com>
2024-03-11 18:04:44 +0800

    feat: 新增csv plugin

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -3,4 +3,23 @@ return {
     "mtdl9/vim-log-highlighting",
   },
   { "prisma/vim-prisma" },
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
 }
