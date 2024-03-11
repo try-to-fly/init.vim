73302c601062cd5d99673103e6d3447892d167bd
微笑 <wang1234561211@outlook.com>
2024-03-11 09:25:28 +0800

    fix: 开启copilot chat debug模式

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,7 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
       show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
-      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      debug = true, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
       language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
       -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
