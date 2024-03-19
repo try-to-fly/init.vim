00176e32029bef5f55bcc0532b3e18cec0999e7b
微笑 <wang1234561211@outlook.com>
2024-03-19 21:09:13 +0800

    feat: grapple.nvim

--- lua/plugins/copy-path.lua
+++ lua/plugins/copy-path.lua
@@ -1,7 +1,11 @@
 return {
   "try-to-fly/project-info.nvim",
-  dependencies = { { "nvim-telescope/telescope.nvim" } },
+  dependencies = {
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-lua/plenary.nvim" },
+  },
   config = function()
     require("telescope").load_extension("copypath")
+    require("telescope").load_extension("search_folders")
   end,
 }
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -30,4 +30,25 @@ return {
       "stevearc/oil.nvim",
     },
   },
+  {
+    "cbochs/grapple.nvim",
+    opts = {
+      scope = "git", -- also try out "git_branch"
+    },
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+    cmd = "Grapple",
+    keys = {
+      { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+      { "<leader>K", "<cmd>Grapple toggle_scopes<cr>", desc = "Grapple toggle scopes" },
+      { "<leader>j", "<cmd>Grapple cycle forward<cr>", desc = "Grapple cycle forward" },
+      { "<leader>J", "<cmd>Grapple cycle backward<cr>", desc = "Grapple cycle backward" },
+      { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple select 1" },
+      { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple select 2" },
+      { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple select 3" },
+      { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple select 4" },
+    },
+  },
 }
