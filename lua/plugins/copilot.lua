043f529ba0b5f1375ed5067cbc30cecd3b08f730
微笑 <wang1234561211@outlook.com>
2024-03-21 13:40:48 +0800

    feat: 修复CopilotChat的输出语言

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
       show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
       debug = true, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
-      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
+      language = "Chinese", -- Copilot answer language settings when using default prompts. Default language is English.
       -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
       -- temperature = 0.1,
     },
