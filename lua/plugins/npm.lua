c0cb02e68effafad8a5732399a344206453154be
微笑 <wang1234561211@outlook.com>
2024-04-15 21:26:52 +0800

    feat: 配置package-info.nvim

--- lua/plugins/npm.lua
+++ lua/plugins/npm.lua
@@ -1,6 +1,7 @@
 return {
   {
     "vuki656/package-info.nvim",
+    event = "BufEnter package.json",
     config = function()
       require("package-info").setup()
       require("telescope").load_extension("package_info")
