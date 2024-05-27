824c20d2d39d97194606319a8900143aa7fe4a88
微笑 <wang1234561211@outlook.com>
2024-05-27 21:09:38 +0800

    feat(plugin): 新增huez.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -33,4 +33,13 @@ return {
   --     colorscheme = "tokyonight",
   --   },
   -- },
+  -- Lazy
+  {
+    "vague2k/huez.nvim",
+    -- if you want registry related features, uncomment this
+    -- import = "huez-manager.import"
+    config = function()
+      require("huez").setup({})
+    end,
+  },
 }
