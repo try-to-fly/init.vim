92b8ec1eef7980d21f4f53801d7225e03a753fb9
微笑 <wang1234561211@outlook.com>
2024-06-13 11:11:36 +0800

    feat: 拆分出playground插件

---
+++ lua/plugins/playground.lua
@@ -0,0 +1,12 @@
+return {
+  "try-to-fly/js-playground.nvim",
+  config = function()
+    require("js_playground").setup_autocmds()
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>r",
+      ":lua require('js_playground').run_js_code()<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+}
