0368e5599942920539cfaaf4a673a4d138fc3129
微笑 <wang1234561211@outlook.com>
2024-06-19 15:50:59 +0800

    fix: 适配新版本gd命令没有被禁用的case

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
+    opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable a keymap
       keys[#keys + 1] = { "K", false }
