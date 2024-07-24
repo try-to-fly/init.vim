b45e11fa8502912f014b91680a63284b796b0ad2
微笑 <wang1234561211@outlook.com>
2024-07-24 15:41:04 +0800

    feat: 临时禁用image插件，避免报错

--- lua/plugins/telescope-media-files.lua
+++ lua/plugins/telescope-media-files.lua
@@ -1,7 +1,8 @@
 return {
   {
     "vhyrro/luarocks.nvim",
-    enabled = vim.loop.os_uname().sysname == "Darwin",
+    -- enabled = vim.loop.os_uname().sysname == "Darwin",
+    enabled = false,
     priority = 1001, -- this plugin needs to run before anything else
     config = true,
     opts = {
@@ -10,7 +11,8 @@ return {
   },
   {
     "3rd/image.nvim",
-    enabled = vim.loop.os_uname().sysname == "Darwin",
+    -- enabled = vim.loop.os_uname().sysname == "Darwin",
+    enabled = false,
     dependencies = { "luarocks.nvim" },
     config = function()
       require("image").setup()
