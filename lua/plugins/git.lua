35acf1de3c7cc8fc9c8048ecc8f0a4633e06b409
微笑 <wang1234561211@outlook.com>
2024-09-18 10:37:29 +0800

    feat: 添加neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -53,4 +53,17 @@ return {
       require("mini.git").setup()
     end,
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+      "echasnovski/mini.pick", -- optional
+    },
+    config = true,
+  },
 }
