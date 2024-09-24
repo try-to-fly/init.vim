af7eafec75fed30e2eaf1629ad38da6542bc8923
微笑 <wang1234561211@outlook.com>
2024-09-24 21:42:29 +0800

    feat: 新增json-path-picker插件

--- lua/plugins/json.lua
+++ lua/plugins/json.lua
@@ -1,19 +1,33 @@
 return {
-  "Myzel394/jsonfly.nvim",
+  {
+    "Myzel394/jsonfly.nvim",
 
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-  },
-  setup = function()
-    require("telescope").load_extension("jsonfly")
-  end,
-  keys = {
-    {
-      "<leader>gj",
-      "<cmd>Telescope jsonfly<cr>",
-      desc = "Open json(fly)",
-      ft = { "json" },
-      mode = "n",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
     },
+    setup = function()
+      require("telescope").load_extension("jsonfly")
+    end,
+    keys = {
+      {
+        "<leader>gj",
+        "<cmd>Telescope jsonfly<cr>",
+        desc = "Open json(fly)",
+        ft = { "json" },
+        mode = "n",
+      },
+    },
+  },
+  {
+    -- dir = "/Users/smile/Documents/try-to-fly/json-path-picker.nvim/",
+    "try-to-fly/json-path-picker.nvim",
+    config = function()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>jp",
+        [[<cmd>lua require('json_path_picker').pick_json_path()<CR>]],
+        { noremap = true, silent = true }
+      )
+    end,
   },
 }
