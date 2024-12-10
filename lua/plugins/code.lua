7b10b38accb0f058e4a6e85f791342f34045ad84
微笑 <wang1234561211@outlook.com>
2024-12-10 11:03:00 +0800

    feat: 使用内置的supermaven插件

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -25,19 +25,4 @@ return {
       },
     },
   },
-  {
-    "nvim-cmp",
-    dependencies = {
-      "supermaven-inc/supermaven-nvim",
-      build = ":SupermavenUseFree", -- remove this line if you are using pro
-      opts = {},
-    },
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "supermaven",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
 }
