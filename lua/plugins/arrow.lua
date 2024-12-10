a46f4bab20b2d59c41789e8239a7ed020157baac
微笑 <wang1234561211@outlook.com>
2024-12-10 10:49:01 +0800

    feat: 修改arrow配置

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -2,6 +2,7 @@ return {
   "otavioschwanck/arrow.nvim",
   opts = {
     show_icons = true,
+    separate_by_branch = true,
     leader_key = ";", -- Recommended to be a single key
     buffer_leader_key = "m", -- Per Buffer Mappings
   },
