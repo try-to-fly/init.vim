f5e0c87dd175de63bc54d5b3fb7e5ba83a893ccd
微笑 <wang1234561211@outlook.com>
2024-12-12 13:23:20 +0800

    feat: 配置dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 2, 2 } },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+          { section = "startup" },
+        },
+      },
+    },
+  },
+}
