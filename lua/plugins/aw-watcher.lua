0eda59cfd1081f6e48fba0cca74ac95cef7c5567
微笑 <wang1234561211@outlook.com>
2024-12-24 14:51:06 +0800

    feat: 新增wakatime统计编码信息

--- lua/plugins/aw-watcher.lua
+++ lua/plugins/aw-watcher.lua
@@ -1,12 +1,15 @@
 return -- lazy.nvim
 {
-  "lowitea/aw-watcher.nvim",
-  opts = { -- required, but can be empty table: {}
-    -- add any options here
-    -- for example:
-    aw_server = {
-      host = "127.0.0.1",
-      port = 5600,
+  {
+    "lowitea/aw-watcher.nvim",
+    opts = { -- required, but can be empty table: {}
+      -- add any options here
+      -- for example:
+      aw_server = {
+        host = "127.0.0.1",
+        port = 5600,
+      },
     },
   },
+  { "wakatime/vim-wakatime", lazy = false },
 }
