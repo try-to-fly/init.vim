d3f0c45ba004fdecb88a118042c1aafd770ee961
微笑 <wang1234561211@outlook.com>
2024-12-25 14:51:59 +0800

    feat: 移除aw-watcher

--- lua/plugins/aw-watcher.lua
+++ lua/plugins/aw-watcher.lua
@@ -1,16 +1,4 @@
 return -- lazy.nvim
 {
-  {
-    "lowitea/aw-watcher.nvim",
-    enabled = vim.loop.os_uname().sysname == "Darwin",
-    opts = { -- required, but can be empty table: {}
-      -- add any options here
-      -- for example:
-      aw_server = {
-        host = "127.0.0.1",
-        port = 5600,
-      },
-    },
-  },
   { "wakatime/vim-wakatime", lazy = false },
 }
