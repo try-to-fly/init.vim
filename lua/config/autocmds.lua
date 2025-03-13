084e580b0c13b7be7f7e46b1d2d6e124f24d78a1
smile <wang1234561211@outlook.com>
2025-03-13 13:25:56 +0800

    feat: 去掉env文件的诊断提示

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,3 +22,14 @@ vim.api.nvim_create_autocmd("FileType", {
 --     end)
 --   end,
 -- })
+
+-- 禁用掉env的诊断提示 via: https://github.com/LazyVim/LazyVim/discussions/4027
+local lsp_hacks = vim.api.nvim_create_augroup("LspHacks", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+  group = lsp_hacks,
+  pattern = ".env*",
+  callback = function(e)
+    vim.diagnostic.enable(false, { bufnr = e.buf })
+  end,
+})
